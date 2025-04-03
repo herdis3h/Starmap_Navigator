cc7b3f01933aaaa519c9379b47c648b6740c4f0b
Herdis <herdishelga@gmail.com>
2025-04-03 15:10:32 +0200

    test

--- app/api/interstellar_destinations/route.ts
+++ app/api/interstellar_destinations/route.ts
@@ -6,9 +6,9 @@ const API_SECRET = process.env.NEXT_API_SECRET
 
 export async function GET(req: NextRequest) {
   const authHeader = req.headers.get('Authorization')
-  if (!authHeader || authHeader !== `Bearer ${API_SECRET}`) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
-  }
+  // if (!authHeader || authHeader !== `Bearer ${API_SECRET}`) {
+  //   return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  // }
 
   const filePath = path.join(process.cwd(), 'app/api/interstellar_destinations/data.json')
   const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'))
