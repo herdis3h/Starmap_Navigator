bfa853469a436714ba7c4698f640cfe4310e147d
Herdis <herdishelga@gmail.com>
2025-04-03 15:28:29 +0200

    123

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import { StarSystem } from '@/types/InterstellarData'
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: false })
 
 async function fetch3DData(): Promise<StarSystem[]> {
-  console.log('Fetching JSON (SERVER-SIDE)')
+  // console.log('Fetching JSON securely... (SERVER-SIDE)', process.env.NEXT_API_SECRET)
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/interstellar_destinations`)
 
-  // if (!res.ok) throw new Error('Failed to fetch JSON')
+  // if (!res.ok) throw new Error('Unauthorized: Failed to fetch JSON')
 
   const data: StarSystem[] = await res.json()
   console.log('JSON Data on the SERVER:', data)
